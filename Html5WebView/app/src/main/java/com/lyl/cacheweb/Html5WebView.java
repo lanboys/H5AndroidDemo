8999007739c00cc9198fb0524ff875dedcc7f739
dsj_lan_bing <lb@gdysj.com.cn>
2018-01-25 18:40:56 +0800

    添加css

--- Html5WebView/app/src/main/java/com/lyl/cacheweb/Html5WebView.java
+++ Html5WebView/app/src/main/java/com/lyl/cacheweb/Html5WebView.java
@@ -2,6 +2,7 @@ package com.lyl.cacheweb;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Message;
 import android.util.AttributeSet;
 import android.webkit.GeolocationPermissions;
@@ -11,6 +12,8 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Set;
 
 /**
  * Wing_Li
@@ -100,7 +103,29 @@ public class Html5WebView extends WebView {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView webView, String url) {
-            webView.loadUrl(url);
+            //http://blog.csdn.net/carson_ho/article/details/64904691
+            // 步骤2：根据协议的参数，判断是否是所需要的url
+            // 一般根据scheme（协议格式） & authority（协议名）判断（前两个参数）
+            //假定传入进来的 url = "js://webview?arg1=111&arg2=222"（同时也是约定好的需要拦截的）
+
+            Uri uri = Uri.parse(url);
+            // 如果url的协议 = 预先约定的 js 协议
+            // 就解析往下解析参数
+            if ("js".equals(uri.getScheme())) {
+                // 如果 authority  = 预先约定协议里的 webview，即代表都符合约定的协议
+                // 所以拦截url,下面JS开始调用Android需要的方法
+                if ("webview".equals(uri.getAuthority())) {
+                    //  步骤3：
+                    // 执行JS所需要调用的逻辑
+                    System.out.println("js调用了Android的方法2");
+                    // 可以在协议上带有参数并传递到Android上
+                    HashMap<String, String> params = new HashMap<>();
+                    Set<String> collection = uri.getQueryParameterNames();
+                }
+            } else {
+                webView.loadUrl(url);
+            }
+
             return true;
         }
     }
